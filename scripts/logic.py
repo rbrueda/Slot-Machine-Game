a622b4a02388e17c9f7561e21bb5ec649ffa4088
Rocio <rbrueda8@gmail.com>
2021-10-25 18:55:40 -0400

    Added Files

---
+++ scripts/logic.py
@@ -0,0 +1,202 @@
+import random
+rich_bebe = 2
+clarabelle = 10
+lobster_bebe = 10
+sleepy_bebe = 10
+sandwich_bebe = 10
+blueberry_bebe = 12
+strawberry_bebe = 12
+cranberry_bebe = 12
+homeless_bebe = 4
+father_bebe = 9
+pickle_bebe = 9
+x = [rich_bebe, clarabelle, lobster_bebe, sleepy_bebe, sandwich_bebe, blueberry_bebe, strawberry_bebe, cranberry_bebe, homeless_bebe, father_bebe, pickle_bebe]
+randomNum = random.randint(0,100)
+print(randomNum)
+var = [0,1,2]
+
+if 1 <= randomNum and randomNum <= 2:
+    print("rich_bebe")
+    var[0] = 1
+elif 3 <= randomNum and randomNum <= 12:
+    print("clarabelle")
+    var[0] = 2
+elif 13 <= randomNum and randomNum <= 22:
+    print("lobster_bebe")
+    var[0] = 3
+elif 23 <= randomNum and randomNum <= 32:
+    print("sleepy_bebe")
+    var[0] = 4
+elif 33 <= randomNum and randomNum <= 42:
+    print("sandwich_bebe")
+    var[0] = 5
+elif 43 <= randomNum and randomNum <= 54:
+    print("blueberry_bebe")
+    var[0] = 6
+elif 55 <= randomNum and randomNum <= 66:
+    print("strawberry_bebe")
+    var[0] = 7
+elif 67 <= randomNum and randomNum <= 78:
+    print("cranberry_bebe")
+    var[0] = 8
+elif 79 <= randomNum and randomNum <= 82:
+    print("homeless_bebe")
+    var[0] = 9
+elif 83 <= randomNum and randomNum <= 91:
+    print("father_bebe")
+    var[0] = 10
+elif 92 <= randomNum and randomNum <= 100:
+    print("pickle_bebe")
+    var[0] = 11
+
+import random
+rich_bebe = 2
+clarabelle = 10
+lobster_bebe = 10
+sleepy_bebe = 10
+sandwich_bebe = 10
+blueberry_bebe = 12
+strawberry_bebe = 12
+cranberry_bebe = 12
+homeless_bebe = 4
+father_bebe = 9
+pickle_bebe = 9
+x = [rich_bebe, clarabelle, lobster_bebe, sleepy_bebe, sandwich_bebe, blueberry_bebe, strawberry_bebe, cranberry_bebe, homeless_bebe, father_bebe, pickle_bebe]
+randomNum = random.randint(0,100)
+print(randomNum)
+
+if 1 <= randomNum and randomNum <= 2:
+    print("rich_bebe")
+    var[1] = 1
+elif 3 <= randomNum and randomNum <= 12:
+    print("clarabelle")
+    var[1] = 2
+elif 13 <= randomNum and randomNum <= 22:
+    print("lobster_bebe")
+    var[1] = 3
+elif 23 <= randomNum and randomNum <= 32:
+    print("sleepy_bebe")
+    var[1] = 4
+elif 33 <= randomNum and randomNum <= 42:
+    print("sandwich_bebe")
+    var[1] = 5
+elif 43 <= randomNum and randomNum <= 54:
+    print("blueberry_bebe")
+    var[1] = 6
+elif 55 <= randomNum and randomNum <= 66:
+    print("strawberry_bebe")
+    var[1] = 7
+elif 67 <= randomNum and randomNum <= 78:
+    print("cranberry_bebe")
+    var[1] = 8
+elif 79 <= randomNum and randomNum <= 82:
+    print("homeless_bebe")
+    var[1] = 9
+elif 83 <= randomNum and randomNum <= 91:
+    print("father_bebe")
+    var[1] = 10
+elif 92 <= randomNum and randomNum <= 100:
+    print("pickle_bebe")
+    var[1] = 11
+
+    import random
+rich_bebe = 2
+clarabelle = 10
+lobster_bebe = 10
+sleepy_bebe = 10
+sandwich_bebe = 10
+blueberry_bebe = 12
+strawberry_bebe = 12
+cranberry_bebe = 12
+homeless_bebe = 4
+father_bebe = 9
+pickle_bebe = 9
+x = [rich_bebe, clarabelle, lobster_bebe, sleepy_bebe, sandwich_bebe, blueberry_bebe, strawberry_bebe, cranberry_bebe, homeless_bebe, father_bebe, pickle_bebe]
+randomNum = random.randint(0,100)
+print(randomNum)
+
+if 1 <= randomNum and randomNum <= 2:
+    print("rich_bebe")
+    var[2] = 1
+elif 3 <= randomNum and randomNum <= 12:
+    print("clarabelle")
+    var[2] = 2
+elif 13 <= randomNum and randomNum <= 22:
+    print("lobster_bebe")
+    var[2] = 3
+elif 23 <= randomNum and randomNum <= 32:
+    print("sleepy_bebe")
+    var[2] = 4
+elif 33 <= randomNum and randomNum <= 42:
+    print("sandwich_bebe")
+    var[2] = 5
+elif 43 <= randomNum and randomNum <= 54:
+    print("blueberry_bebe")
+    var[2] = 6
+elif 55 <= randomNum and randomNum <= 66:
+    print("strawberry_bebe")
+    var[2] = 7
+elif 67 <= randomNum and randomNum <= 78:
+    print("cranberry_bebe")
+    var[2] = 8
+elif 79 <= randomNum and randomNum <= 82:
+    print("homeless_bebe")
+    var[2] = 9
+elif 83 <= randomNum and randomNum <= 91:
+    print("father_bebe")
+    var[2] = 10
+elif 92 <= randomNum and randomNum <= 100:
+    print("pickle_bebe")
+    var[2] = 11
+
+rich_bebe = 1
+clarabelle = 2
+lobster_bebe = 3
+sleepy_bebe = 4
+sandwich_bebe = 5
+blueberry_bebe = 6
+strawberry_bebe = 7
+cranberry_bebe = 8
+homeless_bebe = 9
+father_bebe = 10
+pickle_bebe = 11
+
+
+if  9 <= var[0] <= 11 or 9 <= var[1] <= 11 or 9 <= var[2] <= 11:
+    if var.count(9) == 1: 
+        print("lose ___")
+    elif var.count(9) == 2:
+        print("lose____")
+    elif var.count(9) == 3:
+        print("lose _____")
+    elif var.count(10) or var.count(11) == 1:
+        print("lose ____")   
+    elif var.count(10) or var.count(11) == 2:
+        print("lose ___")    
+    elif var.count(10) or var.count(11) == 3:
+        print("lose ___")
+elif 1 <= var[0] <= 5 or 1 <= var[1] <= 5 or 1 <= var[2] <= 5:
+    if var.count(1) == 1:
+        print("gain ___")
+    elif var.count(1) == 2:
+        print("gain ___")
+    elif var.count(1) == 3:
+        print("gain ___")
+    elif var.count(2) or var.count(3) or var.count(4) or var.count(5) == 1:
+        print("gain ____")
+    elif var.count(2) or var.count(3) or var.count(4) or var.count(5) == 2:
+        print("gain ____")
+    elif var.count(2) or var.count(3) or var.count(4) or var.count(5) == 3:
+        print("gain ____")
+elif 6 <= var[0] <= 8 or 6 <= var[1] <= 8 or 6 <= var[2] <= 8:
+    print("no money gained")
+
+
+# if var[0] == 9 and var[1] == 9 and var[2] == 9:
+#     print("loose ____")
+# elif var[0] == 1 and var[1] == 1 and var[2] == 1:
+#     print("gain ___")
+# elif 2 <= var[0] <= 5 and 2 <= var[1] <= 5 and 2 <= var[2] <= 5:
+#     print("gain ____")
+# elif 10 <= var[0] <= 11 and 10 <= var[1] <= 11 and 10 <= var[2] <= 11:
+#     print("loose ____")
